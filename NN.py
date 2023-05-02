99cabc04208ffd981f3157c355852b99a6acf1ed
Eugene Mak <yujinmak@gmail.com>
2023-05-02 18:41:09 -0400

    added forward propagation

--- NN.py
+++ NN.py
@@ -1,5 +1,28 @@
-REG_LAMBDA = 0.0
+import numpy as np
+import math
 
 
-def forward_propagation():
+def sigmoid(val): return 1 / (1 + math.e ** (-val))  # sigmoid function
+
+
+# computes final output for one training instance
+def forward_propagation(num_layers, thetas, training_inst):
+    print(f"--------------------Training Instance--------------------")
+    # initialize value of first neuron (a^l=1), first layer
+    a = np.array(training_inst["x"])  # x is input, y is output
+    assert (a.ndim == 1)  # ensure 'a' is always a vector. This is so that we know we don't need to transpose
+    for k in range(0, num_layers - 1):  # k+2 is layer num if layers index from 1
+        a = np.insert(a, 0, 1, axis=0)  # insert 1 in front of array, for inserting bias term
+        print(f"a: {a}")
+        print(f"----------Layer number: {k + 2}----------")
+        z = np.matmul(thetas[k], a)
+        print(f"z: {z}")
+        a = sigmoid(z)
+    print(f"final output: {a}")
+    return a
+
+
+def cost(reg_lambda, num_layers, thetas, trainings):
+    for training_inst in trainings:
+        output = forward_propagation(num_layers, thetas, training_inst)
     return 0
--- main.py
+++ main.py
@@ -1,6 +1,25 @@
+import numpy as np
+import NN
+
+
+def run_example(reg_lambda, structure, thetas, trainings):
+    NN.cost(reg_lambda, structure, thetas, trainings)
+    return 0
+
 
 def main():
-    print("bruh")
+    # -----------------Q1-------------------------
+    # each row is a neuron, with its weight and bias
+    # num of rows = num of neurons in next layer
+    # bias term, then weight
+    theta_1 = np.array([[0.40000, 0.10000],
+                        [0.30000, 0.20000]])
+    theta_2 = np.array([[0.70000, 0.50000, 0.60000]])
+    run_example(0.0, len([1, 2, 1]), [theta_1, theta_2],
+                [{"x": [0.13000], "y": [0.90000]}, {"x": [0.42000], "y": [0.23000]}])
+    # -----------------Q2------------------------
+
+    return 0
 
 
 if __name__ == "__main__":
